427e1ef125eef6dc6959ff7b3495f3938d2d3952
pedromachadogoncalves <pedro.machado.goncalves@escola.pr.gov.br>
2026-08-27 08:03:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function ola(){
+alert("voce pegou virus");
+}
+
+function nao(){
+    alert("quem leu e gay");
+}
+function aleatorio(){
+let numeroAleatorio = Math.random();
+alert("o numero e " + numeroAleatorio);
+}
